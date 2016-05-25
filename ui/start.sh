fa15415e6c0a5536f92354dff9a5818309c110d8
yi.wu <yi.wu@baifendian.com>
2016-05-25 20:40:56 +0800

    update ui pass nimbus host

--- ui/start.sh
+++ ui/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+if [ ! -z $NIMBUS_SERVICE_HOST ]; then
+  echo "found nimbus service: $NIMBUS_SERVICE_HOST"
+else
+  echo "no nimbus host found in service env"
+  exit 1
+fi
 
-/storm-nimbus-daemon $@
+/storm-nimbus-daemon $@ --nimbus.host $NIMBUS_SERVICE_HOST
 
 exec bin/storm ui
